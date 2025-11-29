c1a2457ad05c47c0f70e7e95fefb910748dc7835
fillpant <github@nullpointer.click>
2025-11-29 23:19:28 +0000

    Add forgotten assistant role in OpenAIMessageRole.

--- Code/src/main/java/click/nullpointer/genaidafny/openai/completion/common/OpenAIMessageRole.java
+++ Code/src/main/java/click/nullpointer/genaidafny/openai/completion/common/OpenAIMessageRole.java
@@ -3,7 +3,7 @@ package click.nullpointer.genaidafny.openai.completion.common;
 import com.google.gson.annotations.SerializedName;
 
 public enum OpenAIMessageRole {
-    @SerializedName("system") SYSTEM, @SerializedName("developer") DEVELOPER, @SerializedName("user") USER;
+    @SerializedName("system") SYSTEM, @SerializedName("developer") DEVELOPER, @SerializedName("user") USER, @SerializedName("assistant") ASSISTANT;
 
     @Override
     public String toString() {
