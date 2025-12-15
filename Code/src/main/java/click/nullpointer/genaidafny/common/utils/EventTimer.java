d65756b4b9d28a0a3035ad99841b622aaf48850f
fillpant <github@nullpointer.click>
2025-12-15 20:50:27 +0000

    Create EventTimer to track event timings in experiments later.

---
+++ Code/src/main/java/click/nullpointer/genaidafny/common/utils/EventTimer.java
@@ -0,0 +1,63 @@
+package click.nullpointer.genaidafny.common.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EventTimer {
+    private final Map<String, EventTimingRecord> events = new HashMap<>();
+    private transient String lastKey;
+
+    public EventTimingRecord startEvent(String name) {
+        EventTimingRecord record = new EventTimingRecord(System.currentTimeMillis());
+        events.put(name, record);
+        return record;
+    }
+
+    public EventTimingRecord stopEvent(String name) {
+        EventTimingRecord record = events.get(name);
+        if (record != null)
+            return null;
+        record.setEndTimeMs(System.currentTimeMillis());
+        lastKey = name;
+        return record;
+    }
+
+    public EventTimingRecord getLastStoppedEvent() {
+        if (lastKey == null)
+            return null;
+        return events.get(lastKey);
+    }
+
+    public Map<String, EventTimingRecord> getEvents() {
+        return events;
+    }
+
+    public static class EventTimingRecord {
+        private long startTimeMs;
+        private long endTimeMs;
+        private long durationMs;
+
+        public EventTimingRecord(long startTimeMs) {
+            this.startTimeMs = startTimeMs;
+        }
+
+        public long getStartTimeMs() {
+            return startTimeMs;
+        }
+
+        public long getEndTimeMs() {
+            return endTimeMs;
+        }
+
+        public long getDurationMs() {
+            return durationMs;
+        }
+
+        private void setEndTimeMs(long endTimeMs) {
+            this.endTimeMs = endTimeMs;
+            this.durationMs = endTimeMs - startTimeMs;//for serialisation.
+        }
+
+    }
+
+}
